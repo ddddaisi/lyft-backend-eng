d9a0e2b953f651efefe5b9481690e2a5506cb49e
ddddaisi <64975648+ddddaisi@users.noreply.github.com>
2023-01-24 14:48:50 -0500

    refactoring - create car models

---
+++ batteries/spindler_battery.py
@@ -0,0 +1,15 @@
+from battery import Battery
+from datetime import datetime
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date) -> None:
+        super().__init__(last_service_date)
+        self.last_service_date = last_service_date
+    
+    def battery_should_be_serviced(self):
+        service_threshold = self.last_service_date.replace(self.last_service_date + 3)
+
+        if service_threshold < datetime.today().date():
+            return True
+        else:
+            return False
---
+++ engines/sternman_engine.py
@@ -0,0 +1,9 @@
+from engine import Engine
+
+class SternmanEngine(Engine):
+    def __init__(self, warning_light_is_on):
+        super().__init__(0, 0, warning_light_is_on)
+        self.warning_light_is_on = warning_light_is_on
+
+    def engine_should_be_serviced(self):
+        return self.warning_light_is_on
