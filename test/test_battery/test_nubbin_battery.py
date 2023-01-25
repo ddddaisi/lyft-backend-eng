05fb9f81bb2f1ed7158d2bb06ff1a4762ca2b2b6
ddddaisi <64975648+ddddaisi@users.noreply.github.com>
2023-01-25 10:55:59 -0500

    update unit test

---
+++ test/test_battery/test_nubbin_battery.py
@@ -0,0 +1,16 @@
+import unittest
+from datetime import datetime
+from batteries.nubbin_battery import NubbinBattery
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 5)
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 1)
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertFalse(battery.needs_service())
